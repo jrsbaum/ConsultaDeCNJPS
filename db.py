dbaa25d5b6630324279d8f05012d069ab574675c
Júnior Baum <juniorsbaum@hotmail.com>
2023-01-17 22:07:45 -0300

    fix: parametro passado para o banco de dados passava cnpj, agora passa data.

--- db.py
+++ db.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 from cfg import mongo_client_key
 
 
-def insert_to_db(cnpj):
+def insert_to_db(data):
     try:
         client = MongoClient(mongo_client_key)
         db = client.CNPJS
         cnpjs = db.cnpjs
-        cnpjs.insert_one(cnpj)
+        cnpjs.insert_one(data)
     except Exception as e:
         print(f"An error occurred while trying to insert the data in MongoDB: {e}")
 
